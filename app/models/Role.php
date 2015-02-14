0be34cd8d5c4f80cf295a1b1a1f87bb8347c62c2
Jarutphong Somsiri <nihimawari@gmail.com>
2015-02-14 22:40:37 +0700

    convert indents tabs to spaces

--- app/models/Role.php
+++ app/models/Role.php
@@ -2,38 +2,38 @@
 
 class Role extends AbstrctIModel{
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'roles';
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'roles';
 
-	/**
-	 * The database connection used by the model.
-	 *
-	 * @var string
-	 */
-	protected $connection = 'neo4j';
+    /**
+     * The database connection used by the model.
+     *
+     * @var string
+     */
+    protected $connection = 'neo4j';
 
-	// --MASS ASSIGNMENT--
-	/**
-	 * The attributes should be fillable to mass-assignment used by the model.
-	 *
-	 * role name : UPPERCASE
-	 *
-	 * @var array
-	 */
-	protected $fillable = array('name', 'description');
+    // --MASS ASSIGNMENT--
+    /**
+     * The attributes should be fillable to mass-assignment used by the model.
+     *
+     * role name : UPPERCASE
+     *
+     * @var array
+     */
+    protected $fillable = array('name', 'description');
 
-	// --DEFINE RELATIONSHIPS--
-	/**
-	 * a Role model "has many" users.
-	 *
-	 * @var array
-	 * @return object
-	 */
-	public function users() {
-		return $this->hasMany('User');
-	}
+    // --DEFINE RELATIONSHIPS--
+    /**
+     * a Role model "has many" users.
+     *
+     * @var array
+     * @return object
+     */
+    public function users() {
+        return $this->hasMany('User');
+    }
 }
