83cc1bcd7ae4dd67d67ebab06e3668f32c3294b3
Jarutphong Somsiri <nihimawari@gmail.com>
2015-02-14 17:03:14 +0700

    set supermaket kunlasatri font in home & admin page

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -1,7 +1,24 @@
 @extends('home.layout')
 
 @section('content')
-    Home
+    Home<br/>
+    Hello World สารานุกรมกว๊านพะเยา header
+    <h1>Hello World สารานุกรมกว๊านพะเยา ฟอนต์ซูเปอร์มาร์เก็ต</h1>
+    <h2>Hello World สารานุกรมกว๊านพะเยา ฟอนต์ซูเปอร์มาร์เก็ต</h2>
+    <h3>Hello World สารานุกรมกว๊านพะเยา ฟอนต์ซูเปอร์มาร์เก็ต</h3>
+    <h4>Hello World สารานุกรมกว๊านพะเยา ฟอนต์ซูเปอร์มาร์เก็ต</h4>
+    <h5>Hello World สารานุกรมกว๊านพะเยา ฟอนต์ซูเปอร์มาร์เก็ต</h5>
+    <h6>Hello World สารานุกรมกว๊านพะเยา ฟอนต์ซูเปอร์มาร์เก็ต</h6>
+
+    <br/>
+
+    Hello World สารานุกรมกว๊านพะเยา botton class="btn"
+    <h1 class="btn">Hello World สารานุกรมกว๊านพะเยา ฟอนต์กุลสตรี</h1>
+    <h2 class="btn">Hello World สารานุกรมกว๊านพะเยา ฟอนต์กุลสตรี</h2>
+    <h3 class="btn">Hello World สารานุกรมกว๊านพะเยา ฟอนต์กุลสตรี</h3>
+    <h4 class="btn">Hello World สารานุกรมกว๊านพะเยา ฟอนต์กุลสตรี</h4>
+    <h5 class="btn">Hello World สารานุกรมกว๊านพะเยา ฟอนต์กุลสตรี</h5>
+    <h6 class="btn">Hello World สารานุกรมกว๊านพะเยา ฟอนต์กุลสตรี</h6>
 @stop
 
 @section('javascript')
