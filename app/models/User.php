fcba744de05b8008fad6b8667ad8a12811ff4844
Jarutphong Somsiri <nihimawari@gmail.com>
2015-02-15 18:11:44 +0700

    remove lines "define action"

--- app/models/User.php
+++ app/models/User.php
@@ -7,54 +7,46 @@ use Illuminate\Auth\Reminders\RemindableInterface;
 
 class User extends AbstrctIModel implements UserInterface, RemindableInterface {
 
-	use UserTrait, RemindableTrait;
+    use UserTrait, RemindableTrait;
 
-	/**
-	 * The database connection used by the model.
-	 *
-	 * @var string
-	 */
-	protected $connection = 'neo4j';
+    /**
+     * The database connection used by the model.
+     *
+     * @var string
+     */
+    protected $connection = 'neo4j';
 
-	// --MASS ASSIGNMENT--
-	/**
-	 * The attributes should be fillable to mass-assignment used by the model.
-	 *
-	 * @var array
-	 */
-	protected $fillable = array('username', 'avatar', 'email', 'title', 'firstname', 'lastname', 'organization');
+    // --MASS ASSIGNMENT--
+    /**
+     * The attributes should be fillable to mass-assignment used by the model.
+     *
+     * @var array
+     */
+    protected $fillable = array('username', 'avatar', 'email', 'title', 'firstname', 'lastname', 'organization');
 
-	/**
-	 * The attributes should be guarded to mass-assignment used by the model.
-	 *
-	 * @var array
-	 */
-	protected $guarded = array('password');
+    /**
+     * The attributes should be guarded to mass-assignment used by the model.
+     *
+     * @var array
+     */
+    protected $guarded = array('password');
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password');
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = array('password');
 
-	// --DEFINE RELATIONSHIPS--
-	/**
-	 * a User model "belongs to" role.
-	 *
-	 * @var array
-	 * @return object
-	 */
-	public function role() {
-		return $this->belongsTo('Role');
-	}
-
-	// --DEFINE ACTION--
-	/**
-	 * a User model "belongs to" role.
-	 *
-	 * @var array
-	 */
-
+    // --DEFINE RELATIONSHIPS--
+    /**
+     * a User model "belongs to" role.
+     *
+     * @var array
+     * @return object
+     */
+    public function role() {
+        return $this->belongsTo('Role');
+    }
 
 }
