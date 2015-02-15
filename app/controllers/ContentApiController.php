689dd29aea7c0bfe66233c182202cf4ef0b0487b
Chaow Porkaew <pchaowmobile@gmail.com>
2015-02-15 17:38:24 +0700

    add contentApiController class.

---
+++ app/controllers/ContentApiController.php
@@ -0,0 +1,46 @@
+<?php
+
+class ContentApiController extends \BaseController {
+
+    public function __construct(ContentService $contentService) {
+        $this->contentService = $contentService;
+    }
+
+    public function getIndex() {
+
+        $with = ['category'];
+
+        $colFilter = [
+            "title",
+        ];
+
+        $relateColFilter = [
+            'category' => [
+                'name'
+            ]
+        ];
+
+        return  $this->contentService->getPagination("Content",Input::all(),$colFilter,$relateColFilter,$with);
+
+    }
+
+    public function getView($id) {
+        $content = $this->contentService->getById($id);
+        return $content;
+    }
+
+    public  function getEdit($id) {
+        return $this->getView($id);
+    }
+
+    public function postSave() {
+
+        return $this->contentService->save(Input::all());
+    }
+
+    public function postDelete() {
+        return $this->contentService->delete(Input::all());
+
+    }
+
+}
